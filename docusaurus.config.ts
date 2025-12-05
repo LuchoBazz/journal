5853a966fc3081a8f0f520db03b6001bd83d00dd
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2025-12-05 15:24:49 -0500

    feat: Add Home Navigation Link to Navbar and Improve Docusaurus Config Formatting

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -84,16 +84,17 @@ const config: Config = {
         src: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Emoji_u263a.svg',
       },
       items: [
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          position: 'left',
+          label: 'Home',
+          to: 'https://luchobazz.github.io/about-me/',
+          target: '_self',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
         {
           type: 'localeDropdown',
           position: 'right'
-        },
-        {
-          href: 'https://github.com/LuchoBazz/journal',
-          label: 'GitHub',
-          position: 'right',
-        },
+        }
       ],
     },
     footer: {
